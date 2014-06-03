833d2ca5aedfca1fa3f79ff51e985b4047eb9261
Raúl Moya Reyes <raulmoyareyes@hotmail.com>
2014-06-03 17:37:03 +0200

    Creando todos los proyectos.

---
+++ pr2b/src/pr2b/Pr2b.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pr2b;
+
+/**
+ *
+ * @author Raúl
+ */
+public class Pr2b {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
